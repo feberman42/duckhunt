003fd00f8df52b5f034fcba34ae0d06cb518e472
Freddy Gesler <freddy@42berlin.de>
2026-04-26 13:33:28 +0200

    fix

--- flags/looks_weird.c
+++ flags/looks_weird.c
@@ -6,29 +6,30 @@
 /*   By: feberman <feberman@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 12:18:42 by feberman          #+#    #+#             */
-/*   Updated: 2025/11/14 15:25:22 by feberman         ###   ########.fr       */
+/*   Updated: 2026/04/26 13:30:58 by feberman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
-#include <strings.h>
+#include <string.h>
 
 int main(void)
-{ 
+{
 	int	i;
 	const char* text;
-	text = "hello world";
+	text = "hello world"; 
+
 	i = 1;
-	
-	while (i < strlen(text))	
+	while(i < strlen(text))
+	{
+		printf("%.*s\n", i, text);
+	    i++;
+	}
+	while (i>=1)
 	{
     	printf("%.*s\n", i, text) ;
-		i++;
-	}
-	while (i >= 1)
-	{
-	    printf("%.*s\n", i, text);
 		i--;
-	} 
+	}
 	return 0;
+
 }
